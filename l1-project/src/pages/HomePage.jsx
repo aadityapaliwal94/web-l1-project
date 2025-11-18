628fe746955be7ba718dbcfd92f3c66f417c96f4
Aaditya Paliwal <aaditya.paliwal@ccube.com>
2025-11-18 17:47:36 +0530

    feat: Added footer data

--- l1-project/src/pages/HomePage.jsx
+++ l1-project/src/pages/HomePage.jsx
@@ -134,12 +134,12 @@ const HomePage = () => {
                     <button onClick={onBuyButtonTap} className="absolute bottom-5 px-10 w-50 h-12 rounded-ee-full bg-black/75 text-xl text-white active:opacity-50">Buy Now</button>
                     <Dots caresoul_images={caresoul_images} currentIndex={currentIndex} goToSlide={goToSlide} />                                                         
                 </div>                
-                <div> 
+                <div className="pb-10"> 
                     <SectionTitle title="The latest." subTitle="Take a look at what’s new right now." />                   
                     <div className="pl-30 pr-5 pb-18 flex gap-5 overflow-x-scroll flex-nowrap no-scrollbar">
                         {products.map((item, index) => {
                             return (
-                                <ProductCard key={index} product={item} index={index} />
+                                <ProductCard key={"product "+ index} product={item} index={index} />
                             )
                         })}                                                                  
                     </div>
@@ -147,9 +147,37 @@ const HomePage = () => {
                     <div className="pl-30 pr-5 pb-18 flex gap-5 overflow-x-scroll flex-nowrap no-scrollbar">
                         {sponsers_data.map((item, index) => {
                             return (
-                                <SponsersCard key={index} product={item} index={index} />
+                                <SponsersCard key={"sponsers "+ index} product={item} index={index} />
                             )
                         })}                                                                  
+                    </div>
+                    <div className="px-30 pb-10 flex gap-60">
+                        <div className="mb-10">
+                            <img width={120} src="https://lirp.cdn-website.com/76b3fe57/dms3rep/multi/opt/ccube_red-1920w.png" />
+                            <span className="block mt-5">Aaditya Paliwal</span>
+                            <span>Indore, MP - India</span>
+                            
+                        </div>
+                        <div>
+                            <span className="font-bold text-xl">Quick Links</span>
+                            <ul className="pl-1 mt-5">
+                                <a className="cursor-pointer" href="/"><li className="pb-5 hover:text-red-400 hover:underline underline-offset-8">Home</li></a>
+                                <a className="cursor-pointer" href="/"><li className="pb-5 hover:text-red-400 hover:underline underline-offset-8">Store</li></a>
+                                <a className="cursor-pointer" href="/"><li className="pb-5 hover:text-red-400 hover:underline underline-offset-8">About Us</li></a>
+                                <a className="cursor-pointer" href="/"><li className="pb-5 hover:text-red-400 hover:underline underline-offset-8">Contact Us</li></a>                                
+                            </ul>
+                        </div>
+                        <div>
+                            <span className="font-bold text-xl">Contact Information</span>
+                            <span className="block mt-5 text-amber-600">USA</span>
+                            <span className="block mt-5 leading-relaxed">10080 N Wolfe Rd SW3 200 <br/> Cupertino, CA 95014</span>                            
+
+                            <span className="block mt-5 text-amber-600">INDIA</span>
+                            <span className="block mt-5 leading-relaxed">Next Coworks: <br/> AB Rd, Slice 6, Aranya Nagar, Scheme 78,<br/> Vijay Nagar, Indore, Madhya Pradesh 452010</span>                            
+                        </div>                        
+                    </div>
+                    <div className="border-t pt-5 flex justify-center border-gray-300 mx-30 ">
+                        <span>© 2025 All Rights Reserved | Aaditya Paliwal</span>
                     </div>
                 </div>
             </div>            
